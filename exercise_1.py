5d8e0837f3fead47f32bd521152d5d49124c05a3
Jorge <jomontes97@hotmail.com>
2024-03-19 23:24:07 -0400

    Ejercicio Manipulacion de Datos

---
+++ exercise_1.py
@@ -0,0 +1,30 @@
+'''
+
+Imagina que trabajas para una empresa de comercio electrónico y te han proporcionado un conjunto 
+de datos en forma de diccionario que contiene información sobre las transacciones de los clientes. 
+Quieren que realices un análisis básico para comprender mejor el comportamiento de compra de los clientes.
+
+Problema: Utiliza Pandas para responder a las siguientes preguntas sobre las transacciones:
+
+¿Cuántas transacciones hay en total?
+¿Cuál es el monto total de todas las transacciones?
+¿Cuántos clientes únicos han realizado transacciones?
+
+
+'''
+
+transacciones = [
+    {"cliente": "Cliente1", "monto": 100, "fecha": "2024-03-01"},
+    {"cliente": "Cliente2", "monto": 150, "fecha": "2024-03-02"},
+    {"cliente": "Cliente3", "monto": 200, "fecha": "2024-03-03"},
+    {"cliente": "Cliente1", "monto": 120, "fecha": "2024-03-04"},
+    {"cliente": "Cliente4", "monto": 80, "fecha": "2024-03-05"},
+    {"cliente": "Cliente2", "monto": 90, "fecha": "2024-03-06"},
+    {"cliente": "Cliente5", "monto": 300, "fecha": "2024-03-07"},
+    {"cliente": "Cliente3", "monto": 250, "fecha": "2024-03-08"},
+    {"cliente": "Cliente1", "monto": 180, "fecha": "2024-03-09"},
+    {"cliente": "Cliente6", "monto": 200, "fecha": "2024-03-10"}
+]
+
+
+
